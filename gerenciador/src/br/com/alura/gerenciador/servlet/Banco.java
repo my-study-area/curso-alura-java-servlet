61958137d69c99f0d60ee9f210af6eca440bcebd
Adriano Avelino <adriano-st777@hotmail.com>
2021-02-26 15:50:11 -0300

    adiciona a listagem de empresas com Servlets

--- gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Banco.java
@@ -5,6 +5,15 @@ import java.util.List;
 
 public class Banco {
     private static List<Empresa> lista = new ArrayList<>();
+    
+    static {
+    	Empresa empresa1 = new Empresa();
+    	Empresa empresa2 = new Empresa();
+    	empresa1.setNome("Alura");
+    	empresa2.setNome("Caelum");
+    	lista.add(empresa1);
+    	lista.add(empresa2);
+    }
 
     public void adiciona(Empresa empresa) { 
         lista.add(empresa);
