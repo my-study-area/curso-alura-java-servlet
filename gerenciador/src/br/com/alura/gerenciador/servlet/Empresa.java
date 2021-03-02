30d4903a15f9e0240c7c2622ed4efefc80fac12e
Adriano Avelino <adriano-st777@hotmail.com>
2021-03-01 21:31:17 -0300

    adiciona a taglib fmt na listagem de empresas

--- gerenciador/src/br/com/alura/gerenciador/servlet/Empresa.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/Empresa.java
@@ -1,8 +1,11 @@
 package br.com.alura.gerenciador.servlet;
 
+import java.util.Date;
+
 public class Empresa {
 	private int id;
 	private String nome;
+	private Date dataAbertura = new Date();
 
 	public int getId() {
 		return id;
@@ -18,6 +21,14 @@ public class Empresa {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	public Date getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(Date dataAbertura) {
+		this.dataAbertura = dataAbertura;
 	}
 	
 }
