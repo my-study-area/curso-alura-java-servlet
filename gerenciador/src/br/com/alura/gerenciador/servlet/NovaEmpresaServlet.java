3188af9a40fea326a7c51f83b5a5df5461a13a28
Adriano Avelino <adriano-st777@hotmail.com>
2021-03-02 11:33:43 -0300

    adiciona o redirecionamento pelo navegador para evitar cadastro na página de listagem

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -37,11 +37,9 @@ public class NovaEmpresaServlet extends HttpServlet {
 		
         Banco banco = new Banco();
         banco.adiciona(empresa);
-        
-        RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
         request.setAttribute("empresa", empresa.getNome());
-        rd.forward(request, response);
-		
+        
+        response.sendRedirect("listaEmpresas");
 	}
 
 }
